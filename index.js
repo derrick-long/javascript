01f0ede8f62e250ed9b881be43635dadd221d5b4
derrick-long <long.derrick8@gmail.com>
2017-11-15 19:11:34 -0500

    add times

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
+let clickedTime = 0;
+let createdTime = 0;
+let reactionTime = 0;
+
+
 function makeBox(){
+
   let x = Math.random() * 5000;
   x = Math.floor(x);
 
   setTimeout(function() {
     document.getElementById("box").style.visibility ="visible";
+    createdTime = Date.now();
   }, x);
 }
 
 document.getElementById("box").onclick=function(){
+  clickedTime = Date.now();
+  reactionTime = (clickedTime - createdTime)/ 1000;
+  document.getElementById("time").innerHTML = reactionTime;
   this.style.visibility = "hidden";
   makeBox();
 };
